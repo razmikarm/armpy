1420b5d57887e7b56b9b766ab4af85331eb48215
Razmik Poghosyan <razmikarm@gmail.com>
2023-12-17 23:39:52 +0400

    Finalized functions' translation

--- generator/translator.py
+++ generator/translator.py
@@ -6,11 +6,16 @@ from generator.components import Functions, Blocks
 class Translator:
     
     def __init__(self, dictionary = None):
+        self.__functions  = None
+        self.__blocks = None
         self._update_dict(dictionary)
-        self.tree = None
 
     def translate(self, raw_data):
-        pass
+        mid_stage = raw_data
+        for function, trans in self.__functions.items():
+            mid_stage = mid_stage.replace(trans, function)
+        # TODO: Make translation of blocks
+        return mid_stage 
 
     def _update_dict(self, dictionary):
         if dictionary is None:
@@ -18,7 +23,19 @@ class Translator:
         self._load_json(dictionary)
 
     def _load_functions(self, functions_data):
-        pass
+        functions_fields = Functions.get_fields()
+        if len(functions_data) != len(functions_fields):
+            raise Exception("Fields of 'functions' does not match")
+        for field in functions_fields:
+            if field.name not in functions_data:
+                raise Exception(f"Missing function '{field.name}' in dictionary")
+
+        for function, trans in functions_data.items():
+            if not trans:
+                functions_data[function] = function
+        
+        self.__functions = Functions(**functions_data)
+        
 
     def _load_blocks(self, blocks_data):
         pass
